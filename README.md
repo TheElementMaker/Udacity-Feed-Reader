02f28ee29bb01c27f2bd7e27cff338348406b8bc
TheElementMaker <elementmaker99@gmail.com>
2020-04-04 04:15:06 +1100

    First Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Feed Reader Testing
+This project is to test feedReader code using Jasmine.js along with jQuery
+
+# How to run
+* live link :
+* Run index.html in any modern browser
+# Spec's
+* # New Feed Selection
+    * Feed Defined
+       * Make's sure there a value set on allFeeds
+        * Test allFeed making sure there got at lease 1 value
+    * Url is set
+        * Test every item in allFeeds for a url
+    * Feed Names
+        *  Test every item in alLFeeds for a name
+
+* # The Menu
+    * Hidden Body
+        * Making sure the body element has the class menu-hidden
+    * Menu Open
+        * Testing that when menu is open the menu-hidden class is removed and added back when closed
+
+* # Initial Entries
+    * On Screen
+        * Tests to see if there a element displayed on the page
+        
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,61 @@
+$(function(){
+    /*
+        Make sure all feed object is vaild
+    */
+    describe('New Feed Selection', function(){
+        // Spec 1
+        // Test to make sure that feed to not empty and defined
+        it('Feed Defined', function(){
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+        // Spec 2
+        // Testing the url of every feed item to be vaild;
+        it('Url is set', function(){
+            allFeeds.forEach(function(feed){
+                expect(feed.url).toBeTruthy();
+            });
+        });
+        // Spec 3
+        // Testing the name of every feed item to vaild;
+        it('Feed Names', function(){
+            allFeeds.forEach(function(feed){
+                expect(feed.name).toBeTruthy;
+            });
+        });
+    });
+    /*
+        Testing for the menu
+    */
+    describe('The Menu', function(){
+        // Spec 4
+        // Make Sure body is hidden when loading page
+        it('Hidden Body', function(){
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+        // Spec 5
+        // When menu is click does it open
+        it('Menu Open', function(){
+            let menu = $('.menu-icon-link');
+
+            menu.click(); //click to show
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            menu.click(); // click to hide
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+    });
+    /*
+        Test for Initial Entry
+    */
+   describe('Initial Entries', function(){
+        // Spec 6
+        // Test for at least 1 entry
+        beforeEach(function(done){
+            loadFeed(0, done);
+        });
+
+        it('On Screen', function(){
+            expect($('.feed .entry').length).toBeGreaterThan(0);
+        });
+   });
+}());
